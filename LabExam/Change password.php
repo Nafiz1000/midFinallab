42bf2e65172efef3f37746a439fb2e9c6c05f20f
Nafiz1000 <56690613+Nafiz1000@users.noreply.github.com>
2020-02-26 16:54:15 +0600

    Add files via upload

---
+++ LabExam/Change password.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+<form>
+	<fieldset>
+		<legend>Changed Password</legend>
+		<table>
+			<tr>
+			<td>User Id</td>
+		</tr>
+		<tr><td><input type="text" name="" value=""  /></td></tr>
+		<tr>
+			<td>Password</td>
+		</tr>
+		<tr>
+			<td> <input type="password" name="" value=""/></td>
+		</tr>
+		<tr>
+			<td><hr></td>
+		</tr>
+		<tr>
+		
+			<td>
+				<input type="submit" name="" value="Login">
+				<a href="text.html">Register</a>
+			</td>
+		</tr>
+		</table>
+	</fieldset>
+</form>
+</body>
+</html>
+
