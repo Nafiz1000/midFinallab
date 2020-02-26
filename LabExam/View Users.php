4e057b3989ca6e07120712119369612fbe31248d
Nafiz1000 <56690613+Nafiz1000@users.noreply.github.com>
2020-02-26 16:55:03 +0600

    Add files via upload

---
+++ LabExam/View Users.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>view user</title>
+</head>
+<body>
+<table border="1" width:"50%">
+	
+	<tr>
+		<td colspan="5">
+			<center>Users</center>
+		</td>
+	</tr>
+	<tr>
+		<td>ID</td>
+		<td>Name</td>
+		<td>User Type</td>
+	</tr>
+	<tr>
+		<td>15-10101-1</td>
+		<td>Bob</td>
+		<td>Admin</td>
+	</tr>
+	<tr>
+		<td>16-10102-2</td>
+		<td>Anne</td>
+		<td>User</td>
+	</tr>
+	<tr>
+		<td>16-10103-2</td>
+		<td>Kent</td>
+		<td>User</td>
+	</tr>
+	<tr>
+		<td>16-10104-3</td>
+		<td>James</td>
+		<td>Admin</td>
+	</tr>
+	<tr>
+			<td colspan="5" align="right"><a href="Welcome.html">Go Home</a></td>
+			
+		</tr>
+</table>
+</body>
+</html>
---
+++ LabExam/Welcome.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome</title>
+</head>
+<body>
+	<center><h1>Welcome Bob!</h1></center>
+	
+<CENTER>
+	<a href="Welcome.php">Profile</a><br>
+	<a href="Change password.php">Change Password</a><br>
+	<a href="View Users.php">View Users</a><br>
+	<a href="Login.php">Logout</a>
+</CENTER>
+</body>
+</html>
